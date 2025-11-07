f4d158bc18dcf9c25cf22757fa9f0a44157775d4
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-11-07 13:44:58 +0100

    feat: use cdk init app

---
+++ aws-orders/test/aws-orders.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsOrders from '../lib/aws-orders-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-orders-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsOrders.AwsOrdersStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
