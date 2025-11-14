14fe088220639278113745d68455d187bc5b2197
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-11-14 14:34:54 +0100

    feat: use interface for PATCH

--- aws-orders/lambda/confirm-order/index.ts
+++ aws-orders/lambda/confirm-order/index.ts
@@ -1,17 +1,17 @@
 import { DynamoDBClient, UpdateItemCommand } from "@aws-sdk/client-dynamodb"
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
+import { OrderStatus } from "../shared/order"
 
 const client = new DynamoDBClient({})
 const tableName = process.env.ORDERS_TABLE!
+const CONFIRMED: OrderStatus = 'CONFIRMED'
 
 export const handler = async (
     event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
     console.log('PATCH confirm order', JSON.stringify(event))
 
-    const path = event.path
-    const parts = path.split('/')
-    const id = parts[2] // ["", "orders", "{id}", "confirm"]
+    const id = event.pathParameters?.id
 
     if (!id){
         return {
@@ -20,19 +20,29 @@ export const handler = async (
         }
     }
 
-    const commnd = new UpdateItemCommand({
+    try {
+        const commnd = new UpdateItemCommand({
         TableName: tableName,
         Key: { id: { S: id } },
         UpdateExpression: "SET #s = :S",
         ExpressionAttributeNames: { "#s": "status" },
         ExpressionAttributeValues: { ":S": { S: 'CONFIRMED' } },
         ReturnValues: 'UPDATED_NEW'
-    })
+        })
 
-    await client.send(commnd)
+        const response = await client.send(commnd)
+        console.log('Update response', response)
 
-    return {
-        statusCode:200,
-        body: JSON.stringify({ id, message: "order confirmed" })
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ id, message: 'order confirmed' })
+        }
+    }
+    catch (err){
+        console.error('Error confirming order', err)
+        return{
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal server error' })
+        }
     }
 }
